c14a30da889778bf0424fc1fcc5ef6d334c1562e
Mark Harfouche <mark.harfouche@gmail.com>
2023-02-20 10:37:26 -0500

    Try to fixup osx

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,21 +8,29 @@ ln -sf ${GXX} g++
 cp ../xcrun .
 cp ../xcodebuild .
 export PATH=${PWD}:${PATH}
-qmake                         \
-    QMAKE_CXX="${CXX}"        \
-    QMAKE_LINK="${CXX}"       \
-    QMAKE_LFLAGS="${LDFLAGS}" \
-    hello.pro
+if [[ $(uname) == "Linux" ]]; then
+    qmake                         \
+        QMAKE_CXX="${CXX}"        \
+        QMAKE_LINK="${CXX}"       \
+        QMAKE_LFLAGS="${LDFLAGS}" \
+        hello.pro
+else
+    qmake hello.pro
+fi
 make
 ./hello
 # Only test that this builds
 make clean
 
-qmake                         \
-    QMAKE_CXX="${CXX}"        \
-    QMAKE_LINK="${CXX}"       \
-    QMAKE_LFLAGS="${LDFLAGS}" \
-    test_qmimedatabase.pro
+if [[ $(uname) == "Linux" ]]; then
+    qmake                         \
+        QMAKE_CXX="${CXX}"        \
+        QMAKE_LINK="${CXX}"       \
+        QMAKE_LFLAGS="${LDFLAGS}" \
+        test_qmimedatabase.pro
+else
+    qmake test_qmimedatabase.pro
+fi
 make
 ./test_qmimedatabase
 make clean
